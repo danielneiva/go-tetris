90229226e002dd42c802a044a46a3785e222614e
Daniel Neiva <neiva.daniel@outlook.com>
2023-10-14 00:24:36 -0300

    clearing lines and couting points

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/hajimehoshi/ebiten/v2/vector"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 
-	// "image/color"
 	"log"
 )
+
+func repeatingKeyPressed(key ebiten.Key) bool {
+	const (
+		delay    = 30
+		interval = 3
+	)
+
+	d := inpututil.KeyPressDuration(key)
+	if d == 1 {
+		return true
+	}
+	if d >= delay && (d-delay)%interval == 0 {
+		return true
+	}
+	return false
+}
 
 var (
 	err error
@@ -31,6 +51,7 @@ const (
 	BOARD_WIDTH   = 20
 	BOARD_HEIGHT  = 40
 	PADDING       = 200
+	TILE_POINT    = 100
 	TILES_PATH    = "assets/tiles.png"
 )
 
@@ -43,6 +64,7 @@ var BASE_PIECES = map[string]Piece{
 			{0, 0, 0, 0},
 		},
 		&Point{10, 0},
+		&Game{},
 	},
 	"L": {
 		[4][4]int{
@@ -52,6 +74,7 @@ var BASE_PIECES = map[string]Piece{
 			{0, 0, 0, 0},
 		},
 		&Point{10, 0},
+		&Game{},
 	},
 	"J": {
 		[4][4]int{
@@ -61,6 +84,7 @@ var BASE_PIECES = map[string]Piece{
 			{0, 0, 0, 0},
 		},
 		&Point{10, 0},
+		&Game{},
 	},
 	"_": {
 		[4][4]int{
@@ -70,6 +94,7 @@ var BASE_PIECES = map[string]Piece{
 			{1, 1, 1, 1},
 		},
 		&Point{10, 0},
+		&Game{},
 	},
 	"O": {
 		[4][4]int{
@@ -79,6 +104,7 @@ var BASE_PIECES = map[string]Piece{
 			{0, 1, 1, 0},
 		},
 		&Point{10, 0},
+		&Game{},
 	},
 	"Z": {
 		[4][4]int{
@@ -88,6 +114,7 @@ var BASE_PIECES = map[string]Piece{
 			{0, 1, 1, 0},
 		},
 		&Point{10, 0},
+		&Game{},
 	},
 	"S": {
 		[4][4]int{
@@ -97,6 +124,7 @@ var BASE_PIECES = map[string]Piece{
 			{1, 1, 0, 0},
 		},
 		&Point{10, 0},
+		&Game{},
 	},
 }
 
@@ -108,23 +136,63 @@ type Point struct {
 type Piece struct {
 	body  [4][4]int
 	point *Point
+	game  *Game
 }
 
-func (p *Piece) SetFigure(basePiece *Piece) {
-
-}
-func (p *Piece) PrintFigure() {
-
-}
 func (p *Piece) CanRotate() bool {
-	return true
+	rtmp := *p
+	rtmp.Rotate()
+	return !rtmp.IsTouching()
 }
-func (p *Piece) Rotate() {
 
+func (p *Piece) IsTouching() bool {
+	if p.point.Y < 0 || p.point.X < 0 {
+		return true
+	}
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			if p.game.board[i+p.point.X][j+p.point.Y] != 0 && p.body[i][j] != 0 {
+				return true
+			}
+		}
+	}
+	return false
 }
-func (p *Piece) WontTouch(direction int) bool {
-	return true
+
+func (p *Piece) Rotate() {
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			tmp := p.body[3-j][i]
+			p.body[3-j][i] = p.body[3-i][3-j]
+			p.body[3-i][3-j] = p.body[j][3-i]
+			p.body[j][3-i] = p.body[i][j]
+			p.body[i][j] = tmp
+		}
+	}
 }
+func (p *Piece) WillTouch(direction int) bool {
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			isTileFilled := p.body[i][j] != 0
+			switch direction {
+			case Left:
+				if isTileFilled && p.game.board[i+p.point.X-1][j+p.point.Y] != 0 {
+					return true
+				}
+			case Right:
+				if isTileFilled && p.game.board[i+p.point.X+1][j+p.point.Y] != 0 {
+					return true
+				}
+			case Down:
+				if isTileFilled && p.game.board[i+p.point.X][j+p.point.Y+1] != 0 {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
 func (p *Piece) Move(direction int) {
 	switch direction {
 	case Left:
@@ -136,7 +204,21 @@ func (p *Piece) Move(direction int) {
 	}
 }
 func (p *Piece) Descend() {
-	p.point.Y = p.point.Y + 1
+	if !p.WillTouch(Down) {
+		p.point.Y = p.point.Y + 1
+	}
+}
+
+func (p *Piece) AddToGameBoard() {
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			if p.body[i][j] != 0 {
+				p.game.board[i+p.point.X][j+p.point.Y] = 1
+			}
+		}
+	}
+	p.game.CheckForCompleteLines()
+	p.game.GetNewPiece()
 }
 
 type Game struct {
@@ -148,15 +230,18 @@ type Game struct {
 	gameOver      bool
 	sprite        *ebiten.Image
 	updateCounter int
+	font          font.Face
 }
 
 func NewGame() *Game {
+	fmt.Println("Starting game...")
 	var g = &Game{}
 	g.LoadSprites()
+	g.LoadFonts()
 	g.InitBoard()
 	g.GetNewPiece()
 	g.score = 0
-	g.ticks = 0
+	g.ticks = 25
 	g.speed = 25
 	g.gameOver = false
 	return g
@@ -174,11 +259,93 @@ func (g *Game) InitBoard() {
 	}
 }
 
+func (g *Game) LoadFonts() {
+
+	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const dpi = 72
+	g.font, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    24,
+		DPI:     dpi,
+		Hinting: font.HintingVertical,
+	})
+}
+
+func (g *Game) ClearRow(y int) {
+	for i := 1; i < BOARD_WIDTH-1; i++ {
+		g.board[i][y] = 0
+		g.score += TILE_POINT
+	}
+}
+
+func (g *Game) DescendRows(origin int) {
+	rowIsEmpty := true
+	var row []int
+	for i := 1; i < BOARD_WIDTH-1; i++ {
+		row = append(row, g.board[i][origin-1])
+		if g.board[i][origin-1] != 0 {
+			rowIsEmpty = false
+		}
+	}
+	if rowIsEmpty {
+		return
+	}
+	for i := 1; i < BOARD_WIDTH-2; i++ {
+		g.board[i][origin] = row[i-1]
+		g.board[i][origin-1] = 0
+	}
+	g.DescendRows(origin - 1)
+}
+
+func (g *Game) CheckForCompleteLines() {
+	for j := BOARD_HEIGHT - 2; j > 0; j-- {
+		rowIsComplete := true
+		for i := 1; i < BOARD_WIDTH-1; i++ {
+			if g.board[i][j] == 0 {
+				rowIsComplete = false
+				break
+			}
+		}
+		if rowIsComplete {
+			g.ClearRow(j)
+			g.DescendRows(j)
+		}
+	}
+}
+
+func (g *Game) PrintBoard() {
+	for i := 0; i < BOARD_WIDTH; i++ {
+		for j := 0; j < BOARD_HEIGHT; j++ {
+			fmt.Printf("%d ", g.board[i][j])
+		}
+		fmt.Println()
+	}
+	fmt.Println("==============================================================================================================================")
+}
+
 func (g *Game) restart() {
 	g = NewGame()
 }
 
+func (g *Game) DrawScore(screen *ebiten.Image) {
+	msg := fmt.Sprintf("SCORE: %d", g.score)
+	text.Draw(screen, msg, g.font, 100, 100, color.Black)
+}
+
+func (g *Game) CheckFirstLine() {
+	for i := 0; i < BOARD_WIDTH; i++ {
+		if g.board[i][1] != 0 {
+			g.gameOver = true
+			return
+		}
+	}
+}
+
 func (g *Game) GetNewPiece() {
+	g.CheckFirstLine()
 	index := rand.Intn(len(BASE_PIECES))
 	for _, newPiece := range BASE_PIECES {
 		if index == 0 {
@@ -187,6 +354,7 @@ func (g *Game) GetNewPiece() {
 		index--
 	}
 	g.piece.point = &Point{10, 0}
+	g.piece.game = g
 }
 
 func (g *Game) LoadSprites() {
@@ -203,31 +371,43 @@ func (g *Game) Update() error {
 		}
 		return nil
 	}
+
+	if repeatingKeyPressed(ebiten.KeyLeft) {
+		if !g.piece.WillTouch(Left) {
+			g.piece.Move(Left)
+		}
+	}
+	if repeatingKeyPressed(ebiten.KeyUp) {
+		if g.piece.CanRotate() {
+			g.piece.Rotate()
+		}
+	}
+	if repeatingKeyPressed(ebiten.KeyRight) {
+		if !g.piece.WillTouch(Right) {
+			g.piece.Move(Right)
+		}
+	}
+	if repeatingKeyPressed(ebiten.KeyDown) {
+		if !g.piece.WillTouch(Down) {
+			g.piece.Move(Down)
+		}
+	}
+
 	g.updateCounter++
 	//Control speed
 	if g.updateCounter < g.speed {
 		return nil
 	}
 	g.updateCounter = 0
+	if g.piece.WillTouch(Down) {
+		g.piece.AddToGameBoard()
+		return nil
+	}
 	g.piece.Descend()
-
-	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		g.piece.Move(Left)
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		g.piece.Rotate()
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		g.piece.Move(Right)
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		g.piece.Move(Down)
-	}
-
 	return nil
 }
 
-func drawRec(screen *ebiten.Image, x, y, w, h int, clr color.Color) {
+func (g *Game) drawRec(screen *ebiten.Image, x, y, w, h int, clr color.Color, val int) {
 	vector.DrawFilledRect(screen, float32(x*TILE_SIDE+PADDING), float32(y*TILE_SIDE+PADDING), float32(w), float32(h), clr, true)
 	vector.StrokeRect(screen, float32(x*TILE_SIDE+PADDING), float32(y*TILE_SIDE+PADDING), float32(w), float32(h), float32(2), color.RGBA{0, 0, 0, 255}, true)
 }
@@ -237,7 +417,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	for i := 0; i < BOARD_WIDTH; i++ {
 		for j := 0; j < BOARD_HEIGHT; j++ {
 			if g.board[i][j] != 0 {
-				drawRec(screen, i, j, TILE_SIDE, TILE_SIDE, color.RGBA{255, 0, 0, 255})
+				g.drawRec(screen, i, j, TILE_SIDE, TILE_SIDE, color.RGBA{255, 0, 0, 255}, g.board[i][j])
 			}
 		}
 	}
@@ -245,10 +425,12 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 4; j++ {
 			if g.piece.body[i][j] != 0 {
-				drawRec(screen, g.piece.point.X+i, g.piece.point.Y+j, TILE_SIDE, TILE_SIDE, color.RGBA{0, 0, 255, 255})
+				g.drawRec(screen, g.piece.point.X+i, g.piece.point.Y+j, TILE_SIDE, TILE_SIDE, color.RGBA{0, 0, 255, 255}, g.piece.body[i][j])
 			}
 		}
 	}
+
+	g.DrawScore(screen)
 
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(0, 0)
